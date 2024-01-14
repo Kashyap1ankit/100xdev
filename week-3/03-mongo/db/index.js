3a6b0083751db41aaee6f9da61cfa9864dc8e97a
Ankit Kashyap <ankitkashyap6202347735@gmail.com>
2024-01-14 20:11:05 +0530

    Completed the course purchase assignment

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
+const { link } = require("../routes/admin");
+require("dotenv").config();
 
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://kashyap25ankit:fkttCoj7XEV4UXRr@cluster0.01lbg4u.mongodb.net/course_db"
-);
+mongoose.connect(`${process.env.MONGO_URL}`);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
@@ -17,16 +17,56 @@ const AdminSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    min: 8,
   },
+
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
 });
 
 const UserSchema = new mongoose.Schema({
   // Schema definition here
+  username: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  purchasedCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
 });
 
 const CourseSchema = new mongoose.Schema({
   // Schema definition here
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    req: true,
+    min: 1,
+  },
+
+  imageLink: {
+    type: String,
+    req: true,
+  },
 });
 
 const Admin = mongoose.model("Admin", AdminSchema);
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
-const { Admin } = require("../db");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
@@ -13,12 +13,28 @@ router.post("/signup", async (req, res) => {
   res.send("New admin created");
 });
 
-router.post("/courses", adminMiddleware, (req, res) => {
+router.post("/courses", adminMiddleware, async (req, res) => {
   // Implement course creation logic
+  const { title, description, price, imageLink } = req.body;
+  const { username } = req.headers;
+  const newCourse = await new Course({ title, description, price, imageLink });
+  newCourse.save();
+  const coursePoster = await Admin.findOne({ username: username });
+  coursePoster.courses.push(newCourse);
+  coursePoster.save();
+  res.send(`Course created successfully with courseId : ${newCourse._id}`);
 });
 
-router.get("/courses", adminMiddleware, (req, res) => {
+router.get("/courses", adminMiddleware, async (req, res) => {
   // Implement fetching all courses logic
+
+  const { username } = req.headers;
+  const findAdmin = await Admin.findOne({ username: username });
+  const coursesArray = (await findAdmin.populate("courses")).courses;
+  coursesArray.forEach((e) => {
+    e.published = true;
+  });
+  res.send(`courses:[${coursesArray}]`);
 });
 
 module.exports = router;
